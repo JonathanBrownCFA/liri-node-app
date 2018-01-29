1e8d73c365912b51929cf2be40194369c817f83c
debian <debian@debian.debian9os>
2018-01-29 15:49:55 -0500

    first commit

---
+++ keys.js
@@ -0,0 +1,13 @@
+console.log('this is loaded');
+
+exports.twitter = {
+  consumer_key: process.env.FDSz2uZVDXsvaVcwY1vilU09X,
+  consumer_secret: process.env.Vzi6xqh4E6kp4CMwVhPoWE7d6kjTSnwKik6HB4gdU9alMuEspA,
+  access_token_key: process.env.TWITTER_ACCESS_TOKEN_KEY,
+  access_token_secret: process.env.TWITTER_ACCESS_TOKEN_SECRET
+};
+
+exports.spotify = {
+  id: process.env.SPOTIFY_ID,
+  secret: process.env.SPOTIFY_SECRET
+};
