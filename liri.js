36a3e2d708e45ee8f196e78695d59c40a87fbe4c
JonathanBrownCFA <jbrown1@gmail.com>
2018-01-30 16:38:50 -0500

    misc

--- liri.js
+++ liri.js
@@ -1,6 +1,12 @@
 require('dotenv').config()
-var dotenv = require('dotenv').config({path: '/home/debian9os/Bootcamp/nodehw/liri-node-app/.env'})
+// var dotenv = require('dotenv').config({path: '/home/debian9os/Bootcamp/nodehw/liri-node-app/.env'})
 
+console.log(process.env.SPOTIFY_ID)
+console.log(process.env.SPOTIFY_SECRET)
+console.log(process.env.TWITTER_CONSUMER_KEY)
+console.log(process.env.TWITTER_CONSUMER_SECRET)
+console.log(process.env.TWITTER_ACCESS_TOKEN_KEY)
+console.log(process.env.TWITTER_ACCESS_TOKEN_SECRET)
 
-
-
+// var spotify = new Spotify(keys.spotify);
+// var client = new Twitter(keys.twitter);
